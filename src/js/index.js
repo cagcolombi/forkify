fc249b40f91a59f42d4bd6ce48e426182c2c5cd3
gcolombi <gerard@acolyte.ws>
2020-05-14 12:10:51 -0400

    test

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,24 @@
 // GLOBAL APP CONTROLLER
 import Search from "./models/Search";
 
+/* Global state of the app
+* -search object
+* -current recipe object
+* -shopping list object
+* -liked recipes
+*/
+const state = {};
+
+const constrolSearch = () => {
+
+}
+
+document.querySelector('.search').addEventListener('submit', e =>{
+    e.preventDefault();
+    constrolSearch();
+
+});
+
 const search = new Search('pizza');
 console.log(search);
 search.getResults();
